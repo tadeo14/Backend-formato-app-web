c0be748d8eb82d0c89d15304e671e4919e028873
tadeo <tadeoacosta14@gmail.com>
2024-11-13 00:13:18 -0300

    creamos los endpoints

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
-
 const app = express();
+
+
+
+// #creamos  un endpoint 
+app.get('/saludo', (req,res) => {
+		res.send ('Hola saludos desde el backend');
+});
+
+
+
+
 
 app.listen(4000, () => {
     console.log('El servidor se está ejecutando en el puerto 4000');
