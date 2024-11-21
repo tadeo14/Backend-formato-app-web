f0ce97badbeac13b4c27e5fec38c9720541f359b
tadeo <tadeoacosta14@gmail.com>
2024-11-21 00:23:26 -0300

    comienzo a crear los modelos

--- database/config.js
+++ database/config.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect (process.env.DB_CNN);
+        await mongoose.connect(process.env.DB_CNN);
+        console.log('DB CONECTADA');
     } catch (error) {
                 console.log (error);
     }
 };
+
+
+module.exports = dbConnection; 
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+const dbConnection = require('./database/config');
 require('dotenv').config();
 
 
 app.use('/auth', require('./router/authRouter'));
 
-
+dbConnection (); 
 
 app.listen(process.env.PORTLOCAL, () => {
     console.log(`El servidor se está ejecutando en el puerto ${process.env.PORTLOCAL}`);
